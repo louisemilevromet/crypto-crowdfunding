e2f271d94fd299d1d1aba8eda79d07fede7fbf78
Louis-Émile Vromet <levromet5@gmail.com>
2025-01-03 09:57:27 -0500

    fixing

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -8,9 +8,9 @@ import { ConnectButton } from "@rainbow-me/rainbowkit";
 
 export default function Navbar() {
   return (
-    <header className="fixed top-0 z-50 w-full">
-      <div className="max-w-[1082px] mx-auto">
-        <div className="flex mx-4 items-center justify-between p-2 border border-border rounded-full mt-4 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+    <header className="w-full px-4 lg:px-6 flex items-center justify-between fixed top-0 z-50">
+      <div className="max-w-[1082px] w-full mx-auto flex items-center justify-between p-2 border border-border rounded-full mt-4 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+        <div className="flex items-center justify-between w-full">
           <Link href="/" className="flex items-center gap-2">
             <Image src={logo} alt="Logo" width={30} height={30} />
 
