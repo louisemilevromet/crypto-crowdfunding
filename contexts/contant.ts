7750e5ba78c9b98de51f7b527b5399ab1dc7cff8
Louis-Émile Vromet <levromet5@gmail.com>
2025-01-05 20:45:17 -0500

    refactor: using right smartcontract

--- contexts/contant.ts
+++ contexts/contant.ts
@@ -1,4 +1,4 @@
 import crowdFunding from "./CrowdFunding.json";
 
-export const CrowdFundingAddress = "0x00bd0B8fCEc489D887ed2C480D141b3B3a2cA553";
+export const CrowdFundingAddress = "0xeF1459053a78BAFA6e1c80d034E7A68743176827";
 export const CrowdFundingABI = crowdFunding.abi;
